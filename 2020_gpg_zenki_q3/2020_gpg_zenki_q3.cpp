df8a4b57b9a52ecc0a87c6e34f722c037c31e7fe
momokaaoki <momokaaoki0216@gmail.com>
2020-09-01 10:23:07 +0900

    プロジェクト ファイルを追加します。

---
+++ 2020_gpg_zenki_q3/2020_gpg_zenki_q3.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+
+int allCalc(int* i,int* a, int* b){
+    int kasan = 0;  //加算
+    int gensan = 0; //減算
+    int jousan = 0; //乗算    
+    int josan = 0;  //除算
+   
+    if (*i == 0) {
+        return kasan = *a + *b;     //加算の計算
+    }
+    else if(*i == 1) {
+        return gensan = *a - *b;    //減算の計算
+    }
+    else if (*i == 2) {
+        return jousan = *a * *b;    //乗算の計算
+    }
+    else if (*i == 3) {
+        return josan = *a / *b;     //除算の計算
+    }
+      
+}
+
+int main()
+{
+    //計算したい２つの値
+    int a = 3;
+    int b = 3;
+
+    for (int i = 0; i < 4; ++i) {
+        cout << allCalc(&i, &a, &b) << endl;
+    }
+    
+}
+
+
